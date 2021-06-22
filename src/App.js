90d27c81795c3c5b133eaccc6829612872552754
Singhal0123 <71450621+Singhal0123@users.noreply.github.com>
2021-06-22 21:14:29 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import React, { useState } from "react";
+import "./App.css";
+import Select from "react-select";
 
 function App() {
+  var colors = [
+    {
+      value: 1,
+      label: "blue",
+    },
+    {
+      value: 2,
+      label: "red",
+    },
+    {
+      value: 3,
+      label: "black",
+    },
+    {
+      value: 4,
+      label: "#0000ff",
+    },
+    {
+      value: 5,
+      label: "#36F01F ",
+    },
+    {
+      value: 6,
+      label: "#FFA07A",
+    },
+    {
+      value: 7,
+      label: "#E9967A",
+    },
+    {
+      value: 8,
+      label: "#FA8072",
+    },
+    {
+      value: 9,
+      label: "#F08080",
+    },
+    {
+      value: 10,
+      label: "#CD5C5C",
+    },
+  ];
+  var [setbgcolor, bgcolor] = useState(colors.label);
+
+  var handle = (e) => {
+    bgcolor(e.label);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="left">
+        <div className="textbox">
+          <label>Select HEXCODE :🖌️</label>
+        </div>
+        <Select className="select" options={colors} onChange={handle}></Select>
+      </div>
+      <div className="right">
+        <style>{".right {background-color:" + setbgcolor + "}"}</style>
+        <center>
+          <b className="txtcolor">The bgColor id: {setbgcolor}</b>
+        </center>
+      </div>
+    </>
   );
 }
 
